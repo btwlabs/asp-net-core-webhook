c0c7853835d9b5ebd9d4e19c98c3684302e0e512
tedbtwlabs <ted@bythewaylabs.com>
2024-04-02 14:25:51 -0700

    updated some program.cs logging info

--- Program.cs
+++ Program.cs
@@ -3,33 +3,43 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
-
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 // Configure app settings and environment variables
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var app = builder.Build();
+    if (app.Environment.IsDevelopment()) { app.UseDeveloperExceptionPage(); } 
+// Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseDefaultFiles(); // Enable serving up default files
+
+    app.UseStaticFiles(); // Enable serving up static files
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.UseHttpsRedirection();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseDefaultFiles(); // Enable serving up default files
-
-app.UseStaticFiles(); // Enable serving up static files
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    // Write the exception to the console (you may want to use a more sophisticated logging solution for real-world projects).
+    Console.WriteLine(ex.ToString());
+    throw;
+}

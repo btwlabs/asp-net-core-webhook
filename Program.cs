a606528a4a965a4eee4ee6632304dc731b52a18e
tedbtwlabs <ted@bythewaylabs.com>
2024-04-02 15:49:07 -0700

    added windows service binary and setting

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,41 +11,44 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
+
 // Configure app settings and environment variables
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
-try
+// If running as a service, use that.
+if (args.Contains("--service"))
 {
-    var app = builder.Build();
-    if (app.Environment.IsDevelopment()) { app.UseDeveloperExceptionPage(); } 
+    builder.Host.UseWindowsService();
+}
+
+// Build the host
+IHost host = builder.Build();
+
+// Now build the web app
+using var app = builder.Build();
+
 // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
-
-    app.UseDefaultFiles(); // Enable serving up default files
-
-    app.UseStaticFiles(); // Enable serving up static files
-
-    app.UseRouting();
-
-    app.UseAuthorization();
-
-    app.UseHttpsRedirection();
-
-    app.MapControllers();
-
-    app.Run();
-}
-catch (Exception ex)
+if (app.Environment.IsDevelopment())
 {
-    // Write the exception to the console (you may want to use a more sophisticated logging solution for real-world projects).
-    Console.WriteLine(ex.ToString());
-    throw;
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseDefaultFiles(); // Enable serving up default files
+app.UseHttpsRedirection();
+app.UseStaticFiles(); // Enable serving up static files
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+// Run the host
+host.Run();
